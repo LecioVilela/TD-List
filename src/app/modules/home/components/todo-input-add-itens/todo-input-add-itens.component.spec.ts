722d9185d6aea4d6d6d860e6451450238950f16e
LecioVilela <lecio.vilela@yahoo.com>
2022-09-19 22:07:28 -0300

    first commit

---
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoInputAddItensComponent } from './todo-input-add-itens.component';
+
+describe('TodoInputAddItensComponent', () => {
+  let component: TodoInputAddItensComponent;
+  let fixture: ComponentFixture<TodoInputAddItensComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoInputAddItensComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoInputAddItensComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
