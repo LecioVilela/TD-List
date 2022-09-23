3dd4617d7b637c8d2358625b6c786807a37ab66a
LecioVilela <lecio.vilela@yahoo.com>
2022-09-23 18:27:14 -0300

    add sweetalert2

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emmitTask = new EventEmitter();
+
+  public addItemTask: string = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public submitItemTask() {
+    this.emmitTask.emit(this.addItemTask);
+    this.addItemTask = "";
   }
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
+// Interface
+import { TaskList } from './../../model/task-list';
+
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public async setEmitTask(event: string) {
+    await Swal.fire('Atividade adicionada:', event, 'success');
+    this.taskList.push({ task: event, checked: false });
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1)
+  }
+
+  // public async deleteAll() {
+  //   Swal.fire('Ooops?', 'Deseja apagar toda a lista?', 'question').then((result) => {
+  //     if (result.isConfirmed) {
+  //       this.taskList = [];
+  //     }
+  //   }).then(() => {
+  //     Swal.fire('Ótimo!', 'Agora, você pode inserir novas atividades.', 'success');
+  //   });
+  // }
+
+  public async deleteAll() {
+    const result = await Swal.fire('Ooops?', 'Deseja apagar toda a lista? 🤔', 'question')
+    if (result.isConfirmed) {
+      this.taskList = [];
+      await Swal.fire('Ótimo!', 'Agora, você pode inserir novas atividades. 😎', 'success');
+    }
+    else {
+      await Swal.fire('Não finalizou tudo?', 'Quando finalizar é só voltar aqui! 😊', 'info');
+    }
+  }
 }
